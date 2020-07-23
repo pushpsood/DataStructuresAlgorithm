063f05ddd24ee7ef1f93f4215edfd110f226b42d
Pushp Sood <pushpsood27@gmail.com>
2020-07-23 14:53:11 +0530

    LinkedList_InsertingAtGivenPosition

---
+++ linkedlist/InsertingNewNodeLinkedList.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *create(struct Node *start,int x)
+{
+    if(start==NULL)
+        {
+            struct Node *n=new struct Node();
+            n->data=x;
+            n->next=NULL;
+            start=n;
+            return start;
+        }
+    struct Node *s=start;
+    while(s->next!=NULL)
+        s=s->next;
+    struct Node *n=new struct Node();
+    n->data=x;
+    n->next=NULL;
+    s->next=n;
+    return start;
+}
+void display(struct Node *p)
+{
+    while(p)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+    cout<<endl;
+}
+struct Node *Insert(struct Node *p,int pos,int x)
+{
+    struct Node *start=p;
+    struct Node *temp=new struct Node();
+    if(pos==0||p==NULL)
+    {
+        temp->data=x;
+        temp->next=p;
+        return temp;
+    }
+    else if(pos>0)
+    {
+        while(pos-1)
+        {
+            if(p==NULL)
+                return start;
+            pos--;
+            p=p->next;
+        }
+        if(p==NULL)
+                return start;
+        temp->data=x;
+        temp->next=p->next;
+        p->next=temp;
+        return start;
+    }
+    return start;
+};
+int main()
+{
+    struct Node *start=NULL;
+    start=create(start,1);
+    start=create(start,2);
+    start=create(start,3);
+    start=create(start,4);
+    start=create(start,5);
+    start=create(start,6);
+    start=create(start,7);
+    start=create(start,8);
+    display(start);
+    start=Insert(start,0,100);
+    start=Insert(start,5,100);
+    start=Insert(start,10,100);
+    display(start);
+}
