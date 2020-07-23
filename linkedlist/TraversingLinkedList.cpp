6540f7accea7022f98ffcd37995ae714cf28a79a
Pushp Sood <pushpsood27@gmail.com>
2020-07-23 10:27:15 +0530

    LinkedListTraversal

---
+++ linkedlist/TraversingLinkedList.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *create(struct Node *start,int x)
+{
+    if(start==NULL)
+        {
+            struct Node *n=new struct Node();
+            n->data=x;
+            n->next=NULL;
+            start=n;
+            return start;
+        }
+    struct Node *s=start;
+    while(s->next!=NULL)
+        s=s->next;
+    struct Node *n=new struct Node();
+    n->data=x;
+    n->next=NULL;
+    s->next=n;
+    return start;
+}
+void display(struct Node *p)
+{
+    while(p)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+    cout<<endl;
+}
+int main()
+{
+    struct Node *start=NULL;
+    start=create(start,1);
+    start=create(start,2);
+    start=create(start,3);
+    start=create(start,4);
+    start=create(start,5);
+    start=create(start,6);
+    start=create(start,7);
+    start=create(start,8);
+    display(start);
+}
