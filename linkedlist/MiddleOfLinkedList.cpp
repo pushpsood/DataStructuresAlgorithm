ef9e72cb3bf5ae4406692e6687d81fb3b2ff576f
Pushp Sood <pushpsood27@gmail.com>
2020-07-25 23:04:17 +0530

    LinkedList_MiddleElement

--- linkedlist/MiddleOfLinkedList.cpp
+++ linkedlist/MiddleOfLinkedList.cpp
@@ -1,1 +1,65 @@
 
+#include<iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+struct Node *create(struct Node *start,int x)
+{
+    if(start==NULL)
+        {
+            struct Node *n=new struct Node();
+            n->data=x;
+            n->next=NULL;
+            start=n;
+            return start;
+        }
+    struct Node *s=start;
+    while(s->next!=NULL)
+        s=s->next;
+    struct Node *n=new struct Node();
+    n->data=x;
+    n->next=NULL;
+    s->next=n;
+    return start;
+}
+void display(struct Node *p)
+{
+    while(p)
+    {
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+    cout<<endl;
+}
+struct Node * middle(struct Node *p)
+{
+    struct Node *q=p;
+    while(p)
+    {
+        p=p->next;
+        q=q->next;
+        if(p==NULL)
+            break;
+        p=p->next;
+    }
+    return q;
+}
+int main()
+{
+    struct Node *start=NULL;
+    start=create(start,1);
+    start=create(start,2);
+    start=create(start,3);
+    start=create(start,4);
+    start=create(start,5);
+    start=create(start,6);
+    start=create(start,7);
+    start=create(start,8);
+    display(start);
+    struct Node *m=middle(start);
+    cout<<endl<<"Middle Element: "<<m->data;
+
+}
