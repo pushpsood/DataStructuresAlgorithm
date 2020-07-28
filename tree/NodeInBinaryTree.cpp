691d7a83262b8675c9cec1f2e9e36ce7383a37d9
Pushp Sood <pushpsood27@gmail.com>
2020-07-28 23:01:01 +0530

    BinaryTreeClass_NonLeaf_Leaf_Nodes_Degree

---
+++ tree/NodeInBinaryTree.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node *right;
+    Node *left;
+}*root=NULL;
+void create()
+{
+    Node *p,*t;
+    int x;
+    queue<Node *> q;
+    cout<<"Enter root value";
+    cin>>x;
+    root=new Node;
+    root->data=x;
+    root->right=NULL;
+    root->left=NULL;
+    q.push(root);
+    while(!q.empty())
+    {
+        p=q.front();
+        q.pop();
+        cout<<"Enter the left child of "<<p->data<<" (-1=NULL): ";
+        cin>>x;
+        if(x!=-1)
+        {
+            t=new Node;
+            t->left=NULL;
+            t->right=NULL;
+            t->data=x;
+            p->left=t;
+            q.push(t);
+        }
+        cout<<"Enter the right child of "<<p->data<<" (-1=NULL): ";
+        cin>>x;
+        if(x!=-1)
+        {
+            t=new Node;
+            t->left=NULL;
+            t->right=NULL;
+            t->data=x;
+            p->right=t;
+            q.push(t);
+        }
+    }
+}
+int nodes(Node *p)
+{
+    int x=0,y=0;
+    if(p)
+    {
+        x=nodes(p->left);
+        y=nodes(p->right);
+        return x+y+1;
+    }
+    return 0;
+}
+int main()
+{
+    create();
+    cout<<"Number of Nodes\n";
+    cout<<nodes(root);
+}
