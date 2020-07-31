c5de4be8e2c5b145eda503556e9ec8fd0e2b9832
Pushp Sood <pushpsood27@gmail.com>
2020-07-31 16:32:20 +0530

    Hashing_Channing

---
+++ hashing/ChanningHashing.cpp
@@ -0,0 +1,65 @@
+ #include<iostream>
+ using namespace std;
+ class Node
+ {
+ public:
+
+     int data;
+     Node *next;
+ };
+
+Node * SortedInsert(Node *p,int x)
+{
+    if(p==NULL)
+    {
+        Node *t=new Node;
+        t->data=x;
+        t->next=NULL;
+        p=t;
+        return p;
+    }
+    Node *a=p;
+    Node *q;
+    while(p&&(p->data)<x)
+    {
+        q=p;
+        p=p->next;
+    }
+    Node *t=new Node;
+    t->data=x;
+    t->next=q->next;
+    q->next=t;
+    return a;
+    cout<<"gfh";
+}
+
+Insert(Node *A[],int data)
+{
+    int adr=data%10;
+    A[adr]=SortedInsert(A[adr],data);
+}
+
+int main()
+{
+    Node *A[10];
+    for(int i=0;i<10;i++)
+        A[i]=NULL;
+    Insert(A,10);
+    Insert(A,78);
+    Insert(A,15);
+    Insert(A,12);
+    Insert(A,102);
+    Insert(A,102);
+    Insert(A,102);
+
+    for(int i=0;i<10;i++)
+    {
+        Node *p=A[i];
+        while(p)
+        {
+            cout<<p->data<<" ";
+            p=p->next;
+        }
+        cout<<endl;
+    }
+}
