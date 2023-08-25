5640bc60a038c4fb0315950207e310457c39a36b
pushpsood <pushpsood27@gmail.com>
2023-08-25 15:08:13 +0530

    Solved: Construct the Burrows–Wheeler Transform of a String

--- Strings/Programming-Assignment-2/bwt/bwt.cpp
+++ Strings/Programming-Assignment-2/bwt/bwt.cpp
@@ -9,12 +9,38 @@ using std::endl;
 using std::string;
 using std::vector;
 
+string createBWT(const string text)
+{
+    vector<string> ans;
+    string bwt;
+    for(int i=text.length()-1;i>0;i--)
+    {
+        string currentString ;
+        int currentPos = i;
+        while(text[currentPos] != '\0')
+        {
+            currentString += text[currentPos++];
+        }
+        currentPos = 0;
+        while(currentPos<i)
+        {
+            currentString += text[currentPos++];
+        }
+        ans.push_back(currentString);
+    }
+    ans.push_back(text);
+    std::sort(ans.begin(), ans.end());
+    int bwtLoc = text.length()-1;
+    for(auto & an : ans)
+    {
+        bwt += an[bwtLoc];
+    }
+    return bwt;
+}
+
 string BWT(const string& text) {
-  string result = "";
-
-  // write your code here
-
-  return result;
+  string bwt = createBWT(text);
+  return bwt;
 }
 
 int main() {
